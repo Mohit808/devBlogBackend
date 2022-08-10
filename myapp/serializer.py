5a2bb9dabfe31735517ddddb83a0f5c94b77205d
mohit <mohityadav.my58@gmail.com>
2022-08-10 16:13:39 +0530

    "inital"

--- myapp/serializer.py
+++ myapp/serializer.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer,SerializerMethodField
 from rest_framework import generics
 from .models import *
 
+
 class devBlogSer(ModelSerializer):
     class Meta:
         model = devBlog
