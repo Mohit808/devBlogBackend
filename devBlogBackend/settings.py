8b34a90cc39a5cb79b2f422c33cbec07b52407dc
mohit808 <mohityadav.my58@gmail.com>
2022-09-08 11:26:28 +0000

    new changes

--- devBlogBackend/settings.py
+++ devBlogBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vm%k@d$wp)o&fezg0y3!&5p72=)uy+t6^n!06ez&je=zn*tx+%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'https://amityadavayx.pythonanywhere.com']
 
 
 # Application definition
