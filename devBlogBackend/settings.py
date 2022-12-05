3ba2c7c6c43efafedd1720e0037347fdf7d22962
mohit <mohityadav.my58@gmail.com>
2022-12-05 19:19:42 +0530

    cors error

--- devBlogBackend/settings.py
+++ devBlogBackend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework'
+    'rest_framework',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:60070',
+  'https://devblog-8aad2.web.app'
+)
 
 ROOT_URLCONF = 'devBlogBackend.urls'
 
