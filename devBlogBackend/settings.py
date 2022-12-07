29e0e73b09e63caa1d6da17ad00381d4e9734df0
mohit <mohityadav.my58@gmail.com>
2022-12-07 13:55:30 +0530

    static assets

--- devBlogBackend/settings.py
+++ devBlogBackend/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/assets'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
