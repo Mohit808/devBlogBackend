d675454728cf82de1c3286cafba9f9e94492c94a
mohit <mohityadav.my58@gmail.com>
2022-12-07 16:01:30 +0530

    testing

--- devBlogBackend/settings.py
+++ devBlogBackend/settings.py
@@ -130,11 +130,12 @@ STATIC_ROOT = 'static/assets'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
- 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#done by mohit
 
 
 
