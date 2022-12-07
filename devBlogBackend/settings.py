e16eb8b681e5ba1cd0db8961b296e9f4157ed9dd
mohit <mohityadav.my58@gmail.com>
2022-12-07 16:00:20 +0530

    just test

--- devBlogBackend/settings.py
+++ devBlogBackend/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = 'static/assets'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
