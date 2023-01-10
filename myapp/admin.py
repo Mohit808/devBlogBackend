f49d73b7cc8b13cfa63db0e94912bb9a3c71d727
mohit <mohityadav.my58@gmail.com>
2023-01-10 12:51:05 +0530

    admin register

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from myapp.models import devBlog
+from myapp.models import devBlog,StripeDb,StripeServiceDb
 # Register your models here.
 admin.site.register(devBlog)
+admin.site.register(StripeDb)
+admin.site.register(StripeServiceDb)
